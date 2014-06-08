aad2cb49dda5648f005d8421f40d8d29191aa383
Alan Hoffmeister <alanhoffmeister@gmail.com>
2014-06-08 01:46:22 -0300

    Minhas coisas

--- server/lib/sendgrid.js
+++ server/lib/sendgrid.js
@@ -127,6 +127,7 @@ function mailBegin(id){
 }
 
 function mailExceed(id, id_message){
+
     //pegar model Transaction para pegar emails
     //pegar model Message para ver quem excedeu o tempo da carteira.
     var Transaction = mogoose.model('transaction');
